f591bc5f3baf9907f8bf28e05650a863a5d789e5
Akshat Khurana <113327550+Akshatkhurana@users.noreply.github.com>
2024-08-16 02:54:44 +0530

    Add files via upload

---
+++ AddTwoNumbers2.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = carry;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            current->next = new ListNode(sum);
+            current = current->next;
+        }
+        return reverseList(dummy->next);
+    }
+};
+
