978e55357df51116afa98115780b34d7a1d52e64
Akshat Khurana <113327550+Akshatkhurana@users.noreply.github.com>
2024-08-18 15:05:13 +0530

    Add files via upload

---
+++ UglyNumbers2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> dp(n);
+        dp[0] = 1;
+        int ptr1 = 0;
+        int ptr2 = 0;
+        int ptr3 = 0;
+        for(int i=1;i<n;i++) {
+            int two = dp[ptr1]*2;
+            int three = dp[ptr2]*3;
+            int five = dp[ptr3]*5;
+
+            dp[i] = min(two,min(three,five));
+            if(dp[i] == two) {
+                ptr1++;
+            }
+            if(dp[i] == three) {
+                ptr2++;
+            }
+            if(dp[i] == five) {
+                ptr3++;
+            }
+        }
+        return dp[n-1];
+    }
+};
