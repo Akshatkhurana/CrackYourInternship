c03a3f68d990e527cbb08185d855b2f2902925f4
Akshat Khurana <113327550+Akshatkhurana@users.noreply.github.com>
2024-08-29 01:31:46 +0530

    Add files via upload

---
+++ DeleteNodesHavingGreaterValuesOnRight.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void print(Node* root) {
+    Node* temp = root;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* reverse(Node* head) {
+        Node* prev = NULL;
+        Node* curr = head;
+        Node* next = NULL;
+
+        while (curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    Node *compute(Node *head) {
+        
+        head = reverse(head);
+
+        Node* maxNode = head;
+        Node* curr = head;
+
+        while (curr != NULL && curr->next != NULL) {
+            if (curr->next->data < maxNode->data) {
+                Node* temp = curr->next;
+                curr->next = curr->next->next;
+                delete temp; 
+            } else {
+                curr = curr->next;
+                maxNode = curr;
+            }
+        }
+        head = reverse(head);
+
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+        Solution ob;
+        int data = arr[0];
+        Node* head = new Node(data);
+        Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Node* result = ob.compute(head);
+        print(result);
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
